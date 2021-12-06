deaca6a4bbdf6ecaa25888c08eef89a854f62db9
Haoyu-Tan <haoyu.tan97@gmail.com>
2021-12-06 01:32:35 -0600

    implement bubble cursor v1

--- WIMHomeDecoration/Assets/Scripts/RegularGrabber.cs
+++ WIMHomeDecoration/Assets/Scripts/RegularGrabber.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RegularGrabber : Grabber
 {
@@ -11,8 +13,16 @@ public class RegularGrabber : Grabber
     Quaternion grabAngle;
     //Vector3 DeltaPos;
 
+    //bubble cursor
+    private float bubEnableDist;
+    private float bubCheckDist = 0.8f;
+
     [SerializeField] private Transform pointer;
 
+
+    //bubble cursor
+    [SerializeField] private Transform currtRoom;
+    [SerializeField] private Transform bubbleCursor;
 
     //private Material originalGBMaterial;
     //[SerializeField]
@@ -40,12 +50,107 @@ public class RegularGrabber : Grabber
     // Update is called once per frame
     void Update()
     {
+
+        findBubble();
+
         if (grabbedObject)
         {
             
             grabbedObject.GetComponent<DReference>().UpdatePosAng(this.transform.position,this.transform.rotation*Quaternion.Inverse(grabAngle));
             grabAngle = this.transform.rotation;
         }
+    }
+
+    //bubble cusor find target, only checks grabbable
+    GameObject findBubble() {
+        
+        if (currtRoom)
+        {
+            float minDist = Mathf.Infinity;
+            float secMinDist = Mathf.Infinity;
+            Transform firstNeartest = null;
+            Transform secondNeartest = null;
+            foreach (Transform child in currtRoom) {
+                
+                if (!child.gameObject.GetComponent<Grabbable>())
+                    continue;
+
+                BoxCollider bColidr = child.gameObject.GetComponent<BoxCollider>();
+                Vector3 cloestP = bColidr.ClosestPointOnBounds(bubbleCursor.position);
+                float dist = Vector3.Distance(bubbleCursor.position, cloestP);
+
+                if (dist < secMinDist) {
+                    if (dist < minDist)
+                    {
+                        secMinDist = minDist;
+
+                        if (firstNeartest)
+                            secondNeartest = firstNeartest;
+
+
+                        minDist = dist;
+                        firstNeartest = child;
+                    }
+                    else {
+                        secMinDist = dist;
+                        secondNeartest = child;
+                    }
+                }
+            }
+            
+            
+            //minDist longer than threshold
+            if (minDist > bubCheckDist)
+                return null;
+
+            //find farthest dist from bubble cursor to neartest object
+            float minFar = 2*findFarthest(firstNeartest.gameObject);
+
+            
+
+            //second min dist must shorter than threshold
+            if (secMinDist > bubCheckDist)
+            {
+                setBubbleDia(minFar);
+                return firstNeartest.gameObject;
+            }
+
+            //Debug.Log("neartest: " + firstNeartest.gameObject.name + ", second neartest: " + secondNeartest.gameObject.name);
+            //Debug.Log("minFar: " + minFar + ", secMin: " + secMinDist);
+            setBubbleDia(Mathf.Min(minFar, 2*secMinDist - 0.01f));
+            return firstNeartest.gameObject;
+        }
+
+        return null;
+    }
+
+    float findFarthest(GameObject target) {
+
+        BoxCollider colidr = target.GetComponent<BoxCollider>();
+        if (!colidr) return Mathf.Infinity;
+
+        List<Vector3> colVertex = new List<Vector3>();
+        colVertex.Add(colidr.bounds.max);
+        colVertex.Add(colidr.bounds.min);
+        colVertex.Add(new Vector3(colidr.bounds.max.x, colidr.bounds.max.y, colidr.bounds.min.z));
+        colVertex.Add(new Vector3(colidr.bounds.max.x, colidr.bounds.min.y, colidr.bounds.min.z));
+        colVertex.Add(new Vector3(colidr.bounds.max.x, colidr.bounds.min.y, colidr.bounds.max.z));
+        colVertex.Add(new Vector3(colidr.bounds.min.x, colidr.bounds.min.y, colidr.bounds.max.z));
+        colVertex.Add(new Vector3(colidr.bounds.min.x, colidr.bounds.max.y, colidr.bounds.max.z));
+        colVertex.Add(new Vector3(colidr.bounds.min.x, colidr.bounds.max.y, colidr.bounds.min.z));
+
+        float maxDist = 0f;
+        foreach (Vector3 endP in colVertex) {
+            float tempD = Vector3.Distance(bubbleCursor.position, endP);
+            if (tempD > maxDist)
+                maxDist = tempD;
+        }
+
+        return maxDist;
+    }
+
+    void setBubbleDia(float dia) {
+        bubbleCursor.localScale = new Vector3(dia, dia, dia);
     }
 
     public override void Grab(InputAction.CallbackContext context)
